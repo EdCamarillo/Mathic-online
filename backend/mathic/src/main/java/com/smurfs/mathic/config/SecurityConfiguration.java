13fb3d3935213c84186bdb08b4c380ec6a4a8021
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2024-05-26 10:23:41 +0800

    Allow port 3001

--- backend/mathic/src/main/java/com/smurfs/mathic/config/SecurityConfiguration.java
+++ backend/mathic/src/main/java/com/smurfs/mathic/config/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001"));
         configuration.setAllowedMethods(List.of("GET","POST", "OPTIONS", "PUT"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
